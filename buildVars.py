5634d605a78a35a70e2ed5d10d377e12da492b36
titet11 <123260374+titet11@users.noreply.github.com>
2026-05-15 17:32:30 -0600

    Update compatibility for Python 3.13 and NVDA 2026.1 (#23)
    - Bump addon version to 1.7.0
    - Set lastTestedNVDAVersion to 2026.1
    - Set minimumNVDAVersion to 2024.1 for broader compatibility
    - Update CI workflow to Python 3.13 and actions/setup-python@v5
    - Pin markdown==3.7 in CI dependencies
    
    The addon code (bluetoothAudio.py) is pure Python and has no
    Python 3.13 breaking patterns, so no source changes were needed.
    
    Co-authored-by: titet11 <titet11@users.noreply.github.com>

--- buildVars.py
+++ buildVars.py
@@ -1,37 +1,39 @@
-		# -*- coding: UTF-8 -*-
+# -*- coding: UTF-8 -*-
 
 # Build customizations
 # Change this file instead of sconstruct or manifest files, whenever possible.
 
 # Full getext (please don't change)
-_ = lambda x : x
+_ = lambda x: x
 
 # Add-on information variables
 addon_info = {
-	# for previously unpublished addons, please follow the community guidelines at:
-	# https://bitbucket.org/nvdaaddonteam/todo/raw/master/guideLines.txt
-	# add-on Name, internal for nvda
-	"addon_name" : "bluetoothaudio",
-	# Add-on summary, usually the user visible name of the addon.
-	# Translators: Summary for this add-on to be shown on installation and add-on information.
-	"addon_summary" : _("BluetoothAudio"),
-	# Add-on description
-	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-	"addon_description" : _("""Bluetooth Audio is  an NVDA add-on that improves sound quality when working with bluetooth headphones or speakers."""),
-	# version
-	"addon_version" : "1.6.1",
-	# Author(s)
-	"addon_author" : u"Tony Malykh <anton.malykh@gmail.com>",
-	# URL for the add-on documentation support
-	"addon_url" : "https://github.com/mltony/nvda-bluetooth-audio",
-	# Documentation file name
-	"addon_docFileName" : "readme.html",
-	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-	"addon_minimumNVDAVersion" : "2025.1",
-	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion" : "2025.1",
-	# Add-on update channel (default is None, denoting stable releases, and for development releases, use "dev"; do not change unless you know what you are doing)
-	"addon_updateChannel" : None,
+    # for previously unpublished addons, please follow the community guidelines at:
+    # https://bitbucket.org/nvdaaddonteam/todo/raw/master/guideLines.txt
+    # add-on Name, internal for nvda
+    "addon_name": "bluetoothaudio",
+    # Add-on summary, usually the user visible name of the addon.
+    # Translators: Summary for this add-on to be shown on installation and add-on information.
+    "addon_summary": _("BluetoothAudio"),
+    # Add-on description
+    # Translators: Long description to be shown for this add-on on add-on information from add-ons manager
+    "addon_description": _(
+        """Bluetooth Audio is  an NVDA add-on that improves sound quality when working with bluetooth headphones or speakers."""
+    ),
+    # version
+    "addon_version": "1.7.0",
+    # Author(s)
+    "addon_author": "Tony Malykh <anton.malykh@gmail.com>",
+    # URL for the add-on documentation support
+    "addon_url": "https://github.com/mltony/nvda-bluetooth-audio",
+    # Documentation file name
+    "addon_docFileName": "readme.html",
+    # Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
+    "addon_minimumNVDAVersion": "2024.1",
+    # Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
+    "addon_lastTestedNVDAVersion": "2026.1",
+    # Add-on update channel (default is None, denoting stable releases, and for development releases, use "dev"; do not change unless you know what you are doing)
+    "addon_updateChannel": None,
 }
 
 
@@ -39,7 +41,9 @@ import os.path
 
 # Define the python files that are the sources of your add-on.
 # You can use glob expressions here, they will be expanded.
-pythonSources = [os.path.join("addon", "GlobalPlugins", "*.py"), ]
+pythonSources = [
+    os.path.join("addon", "GlobalPlugins", "*.py"),
+]
 
 # Files that contain strings for translation. Usually your python sources
 i18nSources = pythonSources + ["buildVars.py"]
